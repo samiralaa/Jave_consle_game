7754dccd412df1d8a032d2ad241445ccc78d8e3c
Samir Alaa <76245712+samiralaa@users.noreply.github.com>
2025-08-12 15:09:40 +0400

    add more

--- Main.java
+++ Main.java
@@ -5,20 +5,22 @@ abstract class Vehicle {
 }
 
 class Car extends Vehicle {
-    private String name;
+    protected String name;
     private int speed;
     private int fuel;
 
-    public Car(String name, int speed, int fuel)
-    {
-        this.fuel;
-        this.name;
-        this.speed;
+    public Car(String name, int speed, int fuel) {
+        this.fuel = fuel;
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
     }
 
     public void drive() {
-        System.out.println(name + "is driving at" + speed + "km");
-
+        System.out.println(name + " is driving at " + speed + " km/h");
     }
 
     @Override
@@ -36,17 +38,22 @@ class SportCar extends Car {
     public SportCar(String name, int speed, int fuel) {
         super(name, speed, fuel);
     }
+
     @Override
     void start() {
         System.out.println(name + " (SportCar) is starting.");
+    }
+
+    @Override
+    public void drive() {
+        System.out.println(name + " (SportCar) is driving at " + getSpeed() + " km/h.");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-       
-
         SportCar mySportCar = new SportCar("Ferrari", 200, 30);
         mySportCar.start();
+        mySportCar.drive();
     }
 }
